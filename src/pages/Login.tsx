5f1db44f2689f5979e4d6a08de613eca744b5702
Mohammed Rafi <121567736+MohammedRafii@users.noreply.github.com>
2024-04-01 13:18:23 +0530

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,8 +2,9 @@ import AuthCheck from "../component/AuthCheck";
 const Login = () => {
   if (!navigator.cookieEnabled) {
    return <div className="text-4xl text-center">Cookies are currently disabled in your browser. Please enable cookies to use this website.</div>
-  }
+  }else{
   return <AuthCheck />
+  }
 };
 
 export default Login;
