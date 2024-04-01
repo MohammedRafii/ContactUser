c22a3c08675059c1c5a06f50a69f206875adcf4e
Mohammed Rafi <121567736+MohammedRafii@users.noreply.github.com>
2024-04-01 13:15:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./router-config";
 export default function App() {
-
- if (!navigator.cookieEnabled) {
-   return <div className="text-4xl text-center">Cookies are currently disabled in your browser. Please enable cookies to use this website.</div>
- }
-  
-  return <RouterProvider router={router} />
+ return <RouterProvider router={router} />
 }
